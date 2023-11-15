fb848a91dad0cc4b379b7dad5aac780a404e49c5
Josue Castillo <isaac9900.jc@gmail.com>
2023-11-15 12:23:37 -0400

    RN-app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { HomeScreen } from './src/pages/HomeScreen'
+import { StackNavigation } from './src/navigation/StackNavigation';
 
 const App = () => {
   return (
-    <View>
-      <Text>hola</Text>
-    </View>
+    <NavigationContainer>
+      <StackNavigation/>
+    </NavigationContainer>
   )
 }
 
